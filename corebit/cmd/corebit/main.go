dd6d169f7e21e0ef2b1db38d056051414e11d4af
OnescuAlex-Vlad <onescu8779@gmail.com>
2024-09-22 23:55:38 +0300

    feat: Added pulumi and Postgres support

--- corebit/cmd/corebit/main.go
+++ corebit/cmd/corebit/main.go
@@ -1,11 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"github.com/example/corebit/pkg/workflow"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/ec2"
+	"github.com/pulumi/pulumi-aws/sdk/v5/go/aws/rds"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func main() {
-	fmt.Println("Starting CoreBit System...")
-	workflow.RunWorkflow("config.yaml")
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create a security group for the database
+		sg, err := ec2.NewSecurityGroup(ctx, "db-sg", &ec2.SecurityGroupArgs{
+			Description: pulumi.String("Allow database access"),
+			Ingress: ec2.SecurityGroupIngressArray{
+				&ec2.SecurityGroupIngressArgs{
+					Protocol:   pulumi.String("tcp"),
+					FromPort:   pulumi.Int(5432),
+					ToPort:     pulumi.Int(5432),
+					CidrBlocks: pulumi.StringArray{pulumi.String("0.0.0.0/0")},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create a PostgreSQL RDS instance
+		db, err := rds.NewInstance(ctx, "corebit-db", &rds.InstanceArgs{
+			Engine:              pulumi.String("postgres"),
+			InstanceClass:       pulumi.String("db.t3.micro"),
+			AllocatedStorage:    pulumi.Int(20),
+			Name:                pulumi.String("corebit"),
+			Username:            pulumi.String("admin"),
+			Password:            pulumi.String("password123"),
+			VpcSecurityGroupIds: pulumi.StringArray{sg.ID()},
+		})
+		if err != nil {
+			return err
+		}
+
+		// Output database endpoint
+		ctx.Export("dbEndpoint", db.Endpoint)
+
+		return nil
+	})
 }
